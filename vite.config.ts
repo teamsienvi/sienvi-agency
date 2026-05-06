7d08eb0d6c8c8244f3560751d94dcd6b0c883518
Team Sienvi <teamsienvi@gmail.com>
2026-05-06 12:15:55 +0800

    fix(deploy): add start script and allowedHosts for Cloud Run

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
   preview: {
     host: "::",
     port: parseInt(process.env.PORT || "8080"),
+    allowedHosts: true,
   },
   plugins: [
     react(),
